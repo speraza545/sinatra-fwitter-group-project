fe5713fd0ff9ab77420242240a4e02e27dbe2bfd
speraza545 <s.peraza545@gmail.com>
2021-04-11 22:44:26 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
-  enable :sessions
+
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "fwitter_secret"
   end
 
   get "/"  do 
